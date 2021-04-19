80f77e9e88fd3e861e1571f4cc1f6b08c1c4f1ef
Veronique Ziegler <ziegler@jlab.org>
2021-04-19 18:00:09 -0400

    Shift z position of SVT according to target position.

--- common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/SVT/SVTConstants.java
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/SVT/SVTConstants.java
@@ -356,7 +356,7 @@ public class SVTConstants
 			// |			  | radius					  | radius
 			// |			  | 						  |
 			// o==============v===========================v===================================-> z (beamline)
-			System.out.println("SVT READ Z SHIFT VALUE in SVTConstants "+cp.getDouble("/geometry/target/position", 0));
+			//System.out.println("SVT READ Z SHIFT VALUE in SVTConstants "+cp.getDouble("/geometry/target/position", 0));
                         double zShift = cp.getDouble("/geometry/target/position", 0)*10; // units in mm
 			// LAYERRADIUS and ZSTARTACTIVE are used primarily by the Reconstruction and getStrip()
 			for( int region = 0; region < NREGIONS; region++ )
